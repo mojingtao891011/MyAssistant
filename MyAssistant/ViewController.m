c9142f0018d50698a8eb3c7b8c0c55d03c499507
taomojingato <mojingtao@taomojingatodeMacBook-Pro.local>
2015-06-18 10:24:06 +0800

    Initial Commit

---
+++ MyAssistant/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyAssistant
+//
+//  Created by taomojingato on 15/6/18.
+//  Copyright (c) 2015年 mojingato. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MyAssistant/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyAssistant
+//
+//  Created by taomojingato on 15/6/18.
+//  Copyright (c) 2015年 mojingato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
